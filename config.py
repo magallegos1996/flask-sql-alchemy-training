96496083208d50fe740cd39fd8a9590244f834be
unknown <bwcekos@gmail.com>
2021-11-25 11:49:28 -0500

    fix(database navbar): fix a typo in navbar and change the database from mysql to postgresql

--- config.py
+++ config.py
@@ -3,9 +3,10 @@ import os
 
 load_dotenv()
 
-user = os.environ['MYSQL_USER']
-password = os.environ['MYSQL_PASSWORD']
-host = os.environ['MYSQL_HOST']
-database = os.environ['MYSQL_DATABASE']
+user = os.environ['PSSQL_USER']
+password = os.environ['PSSQL_PASSWORD']
+host = os.environ['PSSQL_HOST']
+database = os.environ['PSSQL_DATABASE']
+port = os.environ['PSSQL_PORT']
 
-DATABASE_CONNECTION_URI = f'mysql://{user}:{password}@{host}/{database}'
+DATABASE_CONNECTION_URI = f'postgresql://{user}:{password}@{host}:{port}/{database}'
